02ec3d39be9ecd4c57ec3e68f84c61bf03d26563
Jonathan Watson <thejmdw@gmail.com>
2021-09-14 11:23:38 -0400

    added burger and fries lists

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,7 +1,12 @@
 import React from "react"
 import { Route } from "react-router-dom"
 import { ProfileProvider } from "./auth/ProfileProvider"
+import { BurgerProvider } from "./burger/BurgerProvider"
+import { FriesProvider } from "./fries/FriesProvider"
 import { Profile } from "./auth/Profile"
+import { Menu } from "./menu/Menu"
+import { BurgerList } from "./burger/BurgerList"
+import { FriesList } from "./fries/FriesList"
 
 export const ApplicationViews = () => {
     return <>
@@ -10,11 +15,25 @@ export const ApplicationViews = () => {
             lineHeight: "1.75rem"
         }}> 
             <ProfileProvider>
+            <BurgerProvider>
+            <FriesProvider>
 
                 <Route exact path="/profile">
                     <Profile />
                 </Route>
 
+                <Route exact path="/">
+                    <Menu />
+                </Route>
+                <Route exact path="/burgers">
+                    <BurgerList />
+                </Route>
+                <Route exact path="/fries">
+                    <FriesList />
+                </Route>
+            
+            </FriesProvider>
+            </BurgerProvider>
             </ProfileProvider>
         </main>
     </>
