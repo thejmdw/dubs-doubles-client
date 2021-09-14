7ba34f4588e0baed4e2ae75e181d0ba6e202c4da
Jonathan Watson <thejmdw@gmail.com>
2021-09-14 17:53:26 -0400

    material ui checkboxes added to toppings

--- src/components/burger/BurgerDetail.js
+++ src/components/burger/BurgerDetail.js
@@ -3,6 +3,8 @@ import { BurgerContext } from "./BurgerProvider.js"
 import { LineItemContext } from "../lineitem/LineItemProvider.js"
 // import { EventContext } from "./EventProvider.js"
 import { useHistory, useParams } from "react-router-dom"
+import { Button, Input, Select, MenuItem, InputLabel } from "@material-ui/core"
+import { FormControlLabel, Checkbox } from "@material-ui/core"
 import "./Burger.css"
 
 export const BurgerDetail = () => {
@@ -30,6 +32,24 @@ export const BurgerDetail = () => {
         createLineItem(product)
         // .then(() => history.push(`/burgers/detail/${id}`))
       }
+      const handleControlledCheckChange = e => {
+        // const newPost = { ...post }
+    
+        // // if (newPost.tags.indexOf(parseInt(e.target.value)) > -1) {
+        // //     newPost.tags.splice(newPost.tags.indexOf(parseInt(e.target.value)) - 1, newPost.tags.indexOf(parseInt(e.target.value)))
+        // // } else {
+        // //     newPost.tags.push(parseInt(e.target.value))
+        // // }
+        
+        // // setPost(newPost)
+        // const tagIndex = newPost.tags.indexOf(parseInt(e.target.value))
+        // if (tagIndex > -1) {
+        //   newPost.tags.splice(tagIndex, 1)
+        // } else {
+        // newPost.tags.push(parseInt(e.target.value))
+        // }
+        // setPost(newPost)
+      }
 
     return (
         <>
@@ -39,16 +59,19 @@ export const BurgerDetail = () => {
             </header>
                         <div className="burger__pic">Pic placeholder</div>
                         <div className="burger__description">{burger.description}</div>
-                        <div className="burger__price">${burger.price}</div>
                         {burger.name === "The BYOBurger" ? <div className="burger__toppings">
                             {toppingTypes.map(type => {
-                                return <ul>{type.name}
-                                {toppings.filter(topping => topping.topping_type_id === type.id).map(
-                                    top => {
-                                        return <li>{top.name}</li>
+                                return <ul><h4>{type.name}</h4>
+                                {toppings.filter(topping => topping.topping_type_id === type.id)
+                                    .map(top => {
+                                        return <FormControlLabel
+                                        control={<Checkbox value={top.id} onChange={handleControlledCheckChange} name="topping" />}
+                                        label={top.name}
+                                      />
                                     }
-                                )}</ul>
-                            })}</div> : ""}
+                                    )}</ul>
+                                })}</div> : ""}
+                        <div className="burger__price">${burger.price}</div>
                         <div className="burger__edit">
                         <button className="btn btn-3" onClick={() => {handleAddClick(burger.id)}}>Add to Cart</button>
                         </div>
