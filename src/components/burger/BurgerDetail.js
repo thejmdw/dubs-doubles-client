c347e4700f14d2cc896820d17208b8931cf7c4c4
Jonathan Watson <thejmdw@gmail.com>
2021-09-16 17:47:02 -0400

    added cards for the burgers and fries

--- src/components/burger/BurgerDetail.js
+++ src/components/burger/BurgerDetail.js
@@ -3,9 +3,17 @@ import { BurgerContext } from "./BurgerProvider.js"
 import { LineItemContext } from "../lineitem/LineItemProvider.js"
 // import { EventContext } from "./EventProvider.js"
 import { useHistory, useParams } from "react-router-dom"
-import { Button, Input, Select, MenuItem, InputLabel } from "@material-ui/core"
+import { Input, Select, MenuItem, InputLabel } from "@material-ui/core"
 import { FormControlLabel, Checkbox } from "@material-ui/core"
 import "./Burger.css"
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { css, cx } from '@emotion/react'
+import { flexbox } from "@mui/system"
 
 export const BurgerDetail = () => {
     const history = useHistory()
@@ -62,25 +70,42 @@ export const BurgerDetail = () => {
         <header className="events__header">
                 <h1>{burger?.name}</h1>
             </header>
-                        <div className="burger__pic">Pic placeholder</div>
-                        <div className="burger__description">{burger.description}</div>
-                        {burger.name === "The BYOBurger" ? <div className="burger__toppings">
-                            {toppingTypes.map(type => {
-                                return <ul><h4>{type.name}</h4>
-                                {toppings.filter(topping => topping.topping_type_id === type.id)
-                                    .map(top => {
-                                        return <FormControlLabel
-                                        control={<Checkbox value={top.id} onChange={handleControlledCheckChange} name="topping" />}
-                                        label={`${top.name} $${top.price}`}
-                                      />
-                                    }
-                                    )}</ul>
-                                })}</div> : ""}
-                        <div className="burger__price">${burger.price + buildPrice}</div>
-                        <div className="burger__edit">
-                        <button className="btn btn-3" onClick={() => {handleAddClick(burger.id)}}>Add to Cart</button>
-                        </div>
-                
+            <Card className="burgersCard" >
+                    <CardMedia
+                      component="img"
+                      height="140"
+                      image="https://mui.com/static/images/cards/contemplative-reptile.jpg"
+                      alt="burger photo"
+                    />
+                    <CardContent>
+                      {/* <Typography gutterBottom variant="h5" component="div">
+                        {burger.name}
+                      </Typography>
+                      <Typography variant="h6" color="text.secondary">
+                        ${burger.price}
+                      </Typography> */}
+                      {toppingTypes.map(type => {
+                        return <div><h3>{type.name}</h3><div className="BYOB">
+                        {toppings.filter(topping => topping.topping_type_id === type.id)
+                            .map(top => {
+                                return <FormControlLabel
+                                control={<Checkbox value={top.id} onChange={handleControlledCheckChange} name="topping" />}
+                                label={`${top.name} $${top.price}`}
+                            />
+                            }
+                            )}</div></div>
+                        })}
+                    </CardContent>
+                    <CardActions sx={{
+                        display: 'flex',
+                        justifyContent: 'space-between'
+                    }}>
+                        <div className="BYOB__action">
+                        <div className="burger__price"><h3>${burger.price + buildPrice}</h3></div>
+                      <Button size="large" onClick={() => {handleAddClick(burger.id)}}>Add To Cart</Button>
+                      </div>
+                    </CardActions>
+                  </Card>
             
         </article>
 
@@ -88,7 +113,26 @@ export const BurgerDetail = () => {
         onClick={() => {
             history.push("/Burgers/new")
         }}
-        >Register New Burger</button> */}
+    >Register New Burger</button> */}
 </>
     )
 }
+
+    // <div className="burger__pic">Pic placeholder</div>
+    // <div className="burger__description">{burger.description}</div>
+    // {burger.name === "The BYOBurger" ? <div className="burger__toppings">
+    //     {toppingTypes.map(type => {
+    //         return <ul><h4>{type.name}</h4>
+    //         {toppings.filter(topping => topping.topping_type_id === type.id)
+    //             .map(top => {
+    //                 return <FormControlLabel
+    //                 control={<Checkbox value={top.id} onChange={handleControlledCheckChange} name="topping" />}
+    //                 label={`${top.name} $${top.price}`}
+    //               />
+    //             }
+    //             )}</ul>
+    //         })}</div> : ""}
+    // <div className="burger__price">${burger.price + buildPrice}</div>
+    // <div className="burger__edit">
+    // <button className="btn btn-3" onClick={() => {handleAddClick(burger.id)}}>Add to Cart</button>
+    // </div>
