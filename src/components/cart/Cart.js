35993cea70f8cdc9ed94371f152d2dd1c62ac4b1
Jonathan Watson <thejmdw@gmail.com>
2021-09-17 11:52:01 -0400

    cart styled to resemble a receipt

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -62,37 +62,53 @@ export const Cart = () => {
     return (
         <>
         <article className="Carts">
-        <header className="events__header">
-            <h1>Dub's Doubles</h1>
-            <h3>A Block Near You</h3>
-        </header>
-        <TableContainer component={Paper}>
-            <Table  aria-label="simple table">
-                <TableHead>
+            <header className="events__header">
+                <h1>Dub's Doubles</h1>
+                <h3>A Block Near You</h3>
+            </header>
+            <TableContainer component={Paper}>
+                <Table aria-label="spanning table">
+                    <TableHead>
+                    {/* <TableRow>
+                        <TableCell align="center" colSpan={3}>
+                        Details
+                        </TableCell>
+                        <TableCell align="right">Price</TableCell>
+                    </TableRow> */}
                     <TableRow>
                         <TableCell>Item</TableCell>
-                    
-                        <TableCell align="right">Price</TableCell>
+                        <TableCell align="right">Topping</TableCell>
                         <TableCell align="right"></TableCell>
+                        <TableCell align="right">Price</TableCell>
                     </TableRow>
-                </TableHead>
-                <TableBody>
-                    {cart.lineitems?.map((item) => (
-                        <TableRow
-                        key={item.product.name}
-                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                        >
-                        <TableCell component="th" scope="row">
-                            {item.product.name} 
-                        </TableCell>
-                        
-                        <TableCell align="right">${item.product.price}</TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
-                
+                    </TableHead>
+                    <TableBody>
+                    {cart.lineitems?.map(item => {
+                        return <><TableRow key={item.id}>
+                            <TableCell className="combo__name">{item.product.name}</TableCell>
+                            <TableCell align="right">{item.qty}</TableCell>
+                            <TableCell align="right">{item.unit}</TableCell>
+                            <TableCell align='right'>${item.product.price === 0 ? item.toppings.forEach(topping => {
+                               item.product.price += topping.price}) : item.product.price}</TableCell>
+                            </TableRow>
+                            {item.toppings.length > 0 ? item.toppings.map(topping => {
+                                return <TableRow>
+                                    <TableCell className="combo__name"></TableCell>
+                                    <TableCell align="right">{topping.name}</TableCell>
+                                    <TableCell align="right">{topping.qty}</TableCell>
+                                    <TableCell align='right'>${topping.price}</TableCell>
+                                    </TableRow>
+                            }) : ""}</>
+                    })}
+                    
+                    <TableRow>
+                        <TableCell rowSpan={3} />
+                        <TableCell colSpan={2}>Total</TableCell>
+                        <TableCell align="right">${cart.total}</TableCell>
+                    </TableRow>
+                    </TableBody>
+                </Table>
+            </TableContainer>     
             
         </article>
         
@@ -102,35 +118,104 @@ export const Cart = () => {
         onClick={() => {
             history.push("/Carts/new")
         }}
-        >Register New Cart</button> */}
+    >Register New Cart</button> */}
 </>
     )
 }
-{/* <div className="combo__price">${item.product.price}</div> */}
-{/* <div className="Frie__edit">
-<button className="btn btn-3"
-            onClick={() => history.push(`Combo/edit/${Frie.id}`)}
-            >Edit Frie</button>
-</div> */}
-            //             <div className="Cart__description"><h5>Order #:</h5> {cart.id}</div>
-            //             <div className="Cart__price">Date: {cart.created_date}</div>
-            //             <div className="Cart__price">Customer: {cart.customer?.user.first_name}</div>
-            //             {/* <div className="Cart__price">{cart.customer?.user.first_name}</div> */}
-            //             {
-            //     cart.lineitems?.map(item => {
-            //         return <section key={`combo--${item.id}`} >
-            //             <div className="combo__name">{item.product.name} ${item.product.price === 0 ? item.toppings.forEach(topping => {
-            //                 item.product.price += topping.price}) : item.product.price} <button className="btn btn-3" onClick={() => {handleRemove(item.id)}}>Remove Item</button></div>
-            //             {item.toppings.length > 0 ? item.toppings.map(topping => {
-            //                 return <div> - {topping.name} ${topping.price}<button className="btn btn-3" onClick={() => {handleRemoveAddOn(topping.id, item.id)}}>X</button> </div>
-            //             }) : ""}
-            //         </section>
-            //     })
-            // }
-            //             <div className="Cart__edit">
-            //                 Total: ${cart.total}
-            //             {/* <button className="btn btn-3" onClick={() => {handleAddClick(Cart.id)}}>Add to Cart</button> */}
-            //             </div>
-            //             <div className="Cart__edit">
-            //             <button className="btn btn-3" onClick={() => history.push(`/payment`)}>Payment</button>
-            //             </div>
+//----------- Buttons for CART --------------$$$
+{/* <button className="btn btn-3" onClick={() => {handleRemove(item.id)}}>Remove Item</button> */}
+{/* <button className="btn btn-3" onClick={() => {handleRemoveAddOn(topping.id, item.id)}}>X</button>  */}
+
+
+    // <TableContainer component={Paper}>
+    //     <Table  aria-label="simple table">
+    //         <TableHead>
+    //             <TableRow>
+    //                 <TableCell>Item</TableCell>
+                
+    //                 <TableCell align="right">Price</TableCell>
+    //                 <TableCell align="right"></TableCell>
+    //             </TableRow>
+    //         </TableHead>
+    //         <TableBody>
+    //             {cart.lineitems?.map((item) => (
+    //                 <TableRow
+    //                 key={item.product.name}
+    //                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+    //                 >
+    //                 <TableCell component="th" scope="row">
+    //                     {item.product.name} 
+    //                 </TableCell>
+                    
+    //                 <TableCell align="right">${item.product.price}</TableCell>
+    //                 <TableCell align="right"><button className="btn btn-3" onClick={() => {handleRemove(item.id)}}>X</button></TableCell>
+
+    //                 </TableRow>
+    //             ))}
+    //         </TableBody>
+    //     </Table>
+    // </TableContainer>
+
+    //             <div className="Cart__description"><h5>Order #:</h5> {cart.id}</div>
+    //             <div className="Cart__price">Date: {cart.created_date}</div>
+    //             <div className="Cart__price">Customer: {cart.customer?.user.first_name}</div>
+    //             {/* <div className="Cart__price">{cart.customer?.user.first_name}</div> */}
+    //             {
+        // cart.lineitems?.map(item => {
+        //     return <section key={`combo--${item.id}`} >
+        //         <div className="combo__name">{item.product.name} ${item.product.price === 0 ? item.toppings.forEach(topping => {
+        //             item.product.price += topping.price}) : item.product.price} <button className="btn btn-3" onClick={() => {handleRemove(item.id)}}>Remove Item</button></div>
+        //         {item.toppings.length > 0 ? item.toppings.map(topping => {
+        //             return <div> - {topping.name} ${topping.price}<button className="btn btn-3" onClick={() => {handleRemoveAddOn(topping.id, item.id)}}>X</button> </div>
+        //         }) : ""}
+        //     </section>
+        // })
+    // }
+    //             <div className="Cart__edit">
+    //                 Total: ${cart.total}
+    //             {/* <button className="btn btn-3" onClick={() => {handleAddClick(Cart.id)}}>Add to Cart</button> */}
+    //             </div>
+    //             <div className="Cart__edit">
+    //             <button className="btn btn-3" onClick={() => history.push(`/payment`)}>Payment</button>
+    //             </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
