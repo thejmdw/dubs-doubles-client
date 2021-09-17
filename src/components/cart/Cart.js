fed6f0eb9725322c809a44dc5ca2c09c1514308c
Jonathan Watson <thejmdw@gmail.com>
2021-09-16 20:12:27 -0400

    cart now reads as a table. need to add lines for BYOB items

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -4,6 +4,13 @@ import { LineItemContext } from "../lineitem/LineItemProvider.js"
 // import { EventContext } from "./EventProvider.js"
 import { useHistory, useParams } from "react-router-dom"
 import "./Cart.css"
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 export const Cart = () => {
     const history = useHistory()
@@ -56,31 +63,35 @@ export const Cart = () => {
         <>
         <article className="Carts">
         <header className="events__header">
-                <h1>Dub's Doubles</h1>
-                <h3>A Block Near You</h3>
-            </header>
-                        <div className="Cart__description">Order #: {cart.id}</div>
-                        <div className="Cart__price">Date: {cart.created_date}</div>
-                        <div className="Cart__price">Customer: {cart.customer?.user.first_name}</div>
-                        <div className="Cart__price">{cart.customer?.user.first_name}</div>
-                        {
-                cart.lineitems?.map(item => {
-                    return <section key={`combo--${item.id}`} >
-                        <div className="combo__name">{item.product.name} ${item.product.price === 0 ? item.toppings.forEach(topping => {
-                            item.product.price += topping.price}) : item.product.price} <button className="btn btn-3" onClick={() => {handleRemove(item.id)}}>Remove Item</button></div>
-                        {item.toppings.length > 0 ? item.toppings.map(topping => {
-                            return <div> - {topping.name} ${topping.price}<button className="btn btn-3" onClick={() => {handleRemoveAddOn(topping.id, item.id)}}>X</button> </div>
-                        }) : ""}
-                    </section>
-                })
-            }
-                        <div className="Cart__edit">
-                            Total: ${cart.total}
-                        {/* <button className="btn btn-3" onClick={() => {handleAddClick(Cart.id)}}>Add to Cart</button> */}
-                        </div>
-                        <div className="Cart__edit">
-                        <button className="btn btn-3" onClick={() => history.push(`/payment`)}>Payment</button>
-                        </div>
+            <h1>Dub's Doubles</h1>
+            <h3>A Block Near You</h3>
+        </header>
+        <TableContainer component={Paper}>
+            <Table  aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Item</TableCell>
+                    
+                        <TableCell align="right">Price</TableCell>
+                        <TableCell align="right"></TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {cart.lineitems?.map((item) => (
+                        <TableRow
+                        key={item.product.name}
+                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                        <TableCell component="th" scope="row">
+                            {item.product.name} 
+                        </TableCell>
+                        
+                        <TableCell align="right">${item.product.price}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
                 
             
         </article>
@@ -101,3 +112,25 @@ export const Cart = () => {
             onClick={() => history.push(`Combo/edit/${Frie.id}`)}
             >Edit Frie</button>
 </div> */}
+            //             <div className="Cart__description"><h5>Order #:</h5> {cart.id}</div>
+            //             <div className="Cart__price">Date: {cart.created_date}</div>
+            //             <div className="Cart__price">Customer: {cart.customer?.user.first_name}</div>
+            //             {/* <div className="Cart__price">{cart.customer?.user.first_name}</div> */}
+            //             {
+            //     cart.lineitems?.map(item => {
+            //         return <section key={`combo--${item.id}`} >
+            //             <div className="combo__name">{item.product.name} ${item.product.price === 0 ? item.toppings.forEach(topping => {
+            //                 item.product.price += topping.price}) : item.product.price} <button className="btn btn-3" onClick={() => {handleRemove(item.id)}}>Remove Item</button></div>
+            //             {item.toppings.length > 0 ? item.toppings.map(topping => {
+            //                 return <div> - {topping.name} ${topping.price}<button className="btn btn-3" onClick={() => {handleRemoveAddOn(topping.id, item.id)}}>X</button> </div>
+            //             }) : ""}
+            //         </section>
+            //     })
+            // }
+            //             <div className="Cart__edit">
+            //                 Total: ${cart.total}
+            //             {/* <button className="btn btn-3" onClick={() => {handleAddClick(Cart.id)}}>Add to Cart</button> */}
+            //             </div>
+            //             <div className="Cart__edit">
+            //             <button className="btn btn-3" onClick={() => history.push(`/payment`)}>Payment</button>
+            //             </div>
---
+++ src/components/theme.js
@@ -0,0 +1,13 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+export const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#9760ca',
+      main: '#663399'
+    },
+    secondary: {
+      main: '#663399',  
+    },
+  },
+});
